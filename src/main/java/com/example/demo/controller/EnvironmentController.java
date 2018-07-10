59e622eab552ee2f63c9c043ed758ec44fd7e1f4
roberto aban soliz <robertoabanzoliz@gmail.com>
2018-07-09 20:34:57 -0400

    perfiles

---
+++ src/main/java/com/example/demo/controller/EnvironmentController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EnvironmentController {
+
+    @Autowired
+    private Environment environment;
+    @GetMapping("/environment")
+    public String env() {
+        return "Estoy en el ambiente: " + environment.getActiveProfiles()[0];
+    }
+}
